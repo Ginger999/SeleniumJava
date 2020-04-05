61a8d9d24a4568ff52c61c580694b1ceadf3952b
Ginger999 <sansara_@bk.ru>
2020-04-05 14:23:19 +0700

    Add: Scan Menu items

--- SeleniumJava/src/test/java/Test_02_Login.java
+++ SeleniumJava/src/test/java/Test_02_Login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,9 +13,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -55,20 +61,36 @@ public class Test_02_Login {
         driver = null;
     }
 
+//    @Test
+//    public void TestLinkClick() {
+//        driver.get(website);
+//        driver.findElement(By.xpath("//*[@id=\"box-login\"]/div/a/img")).click();
+//        wait.until(titleIs("Online Store | My Store"));
+//    }
     @Test
-    public void TestLinkClick() {
-        driver.get(website);
-        driver.findElement(By.xpath("//*[@id=\"box-login\"]/div/a/img")).click();
-        wait.until(titleIs("Online Store | My Store"));
-    }
+    public void TestMenu() {
+        Actions actions = new Actions(driver);
 
-    @Test
-    public void TestLogin() {
         driver.get(website);
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("remember_me")).click();
         driver.findElement(By.name("login")).click();
         wait.until(titleIs("My Store"));
+
+        List<WebElement> MenuItems = driver.findElements(By.cssSelector("ul#box-apps-menu li#app-"));
+        System.out.println(MenuItems.size());
+        int MenuItemsCount = MenuItems.size();
+        for (int i = 1; i < MenuItemsCount; i++) {
+            String si = String.valueOf(i);
+            String cssSelector = "ul#box-apps-menu li:nth-child(" + si + ")";
+            System.out.print(si + ": " + cssSelector + " | ");
+            //cssSelector = "ul#box-apps-menu li:nth-child(1)"
+            //WebElement MenuItem = driver.findElement(By.cssSelector(cssSelector));
+            WebElement MenuItem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(cssSelector)));
+            actions.moveToElement(MenuItem).build().perform();
+            //MenuItem.click();
+            System.out.println(MenuItem.getText());
+        }
     }
 }
